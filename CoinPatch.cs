79ce83d16986b9ba16c2b748539d415d2d8dbb44
Bram Vernimmen <vernimmenbram@gmail.com>
2024-03-11 16:28:14 +0100

    Initial Commit of Project

---
+++ CoinPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RespawningCoins
+{
+    [HarmonyPatch(typeof(Coin), "get_AlwaysRespawn")]
+    public class CoinPatch
+    {
+        [HarmonyPrefix]
+        public static bool AlwaysRespawnPreFix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace RespawningCoins
+{
+    [BepInPlugin("pro192.uch.RespawningCoins", "Respawning Coins", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
